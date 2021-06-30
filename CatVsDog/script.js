c55fa0eafa001f7a198acab6103990408a8a7561
Svetie <62491496+Svetie@users.noreply.github.com>
2021-06-29 23:20:08 -0400

    change image onclick

---
+++ CatVsDog/script.js
@@ -0,0 +1,9 @@
+let animal = document.querySelector('#animal');
+
+function pickCat(element){
+    animal.src = "images/cat.jpeg";
+}
+
+function pickDog(element){
+    animal.src = "images/dog.jpg";
+}
